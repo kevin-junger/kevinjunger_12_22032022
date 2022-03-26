b72e59dbaf79a4c854a0f718f4a90cbeb55b83ff
Kevin Junger <kevin.junger@outlook.fr>
2022-03-26 17:54:13 +0100

    documentation jsdoc + passage init() en private

--- front/src/Api.jsx
+++ front/src/Api.jsx
@@ -1,12 +1,21 @@
 import Axios from "axios"
 
+/**
+ * @class Api
+ * @classdesc Manages HTTP GET calls in order to fetch the necessary data
+ */
+
 export default class Api {
   constructor() {
     this.baseUrl = "http://localhost:3000"
     this.fetch = null
   }
 
-  init() {
+  /**
+   * Creates an Axios instance which allows the getters to get the desired data
+   * @returns AxiosInstance
+   */
+  #init() {
     let headers = {
       Accept: "application/json",
     }
@@ -19,27 +28,57 @@ export default class Api {
     return this.fetch
   }
 
+  /**
+   * Returns an Axios response with the user's information
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUser(userId) {
-    return this.init().get(`/user/${userId}`)
+    return this.#init().get(`/user/${userId}`)
   }
 
+  /**
+   * Returns an Axios response with the user's daily activity
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUserActivity(userId) {
-    return this.init().get(`/user/${userId}/activity`)
+    return this.#init().get(`/user/${userId}/activity`)
   }
 
+  /**
+   * Returns an Axios response with the average duration of the user's sessions
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUserAverageSessions(userId) {
-    return this.init().get(`/user/${userId}/average-sessions`)
+    return this.#init().get(`/user/${userId}/average-sessions`)
   }
 
+  /**
+   * Returns an Axios response with the user's daily goal information
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUserTodayScore(userId) {
-    return this.init().get(`/user/${userId}/today-score`)
+    return this.#init().get(`/user/${userId}/today-score`)
   }
 
+  /**
+   * Returns an Axios response with the types of activities chosen by the user
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUserActivities(userId) {
-    return this.init().get(`/user/${userId}/activities`)
+    return this.#init().get(`/user/${userId}/activities`)
   }
 
+  /**
+   * Returns an Axios response with the user's key data (calories, proteins, etc.)
+   * @param { Integer } userId 
+   * @returns Promise
+   */
   getUserKeyData(userId) {
-    return this.init().get(`/user/${userId}/key-data`)
+    return this.#init().get(`/user/${userId}/key-data`)
   }
 }
