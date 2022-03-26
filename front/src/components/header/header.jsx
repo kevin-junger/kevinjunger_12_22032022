82610d2879cfe296dff8c993580cb03cc200482e
Kevin Junger <kevin.junger@outlook.fr>
2022-03-26 17:21:46 +0100

    creation header

---
+++ front/src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import "./header.css"
+import logo from "./logo.png"
+
+export default function Header() {
+  return(
+    <header className="header">
+      <nav className="navbar">
+        <ul className="navbar__menu">
+          <li>
+            <img className="navbar__logo" src={logo} alt="logo" />
+          </li>
+          <li>Accueil</li>
+          <li>Profil</li>
+          <li>Réglage</li>
+          <li>Communauté</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
