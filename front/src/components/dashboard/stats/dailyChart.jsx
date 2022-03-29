dce3c2e6a51e9f6a40c85ed551e4d6ce1dcc4104
Kevin Junger <kevin.junger@outlook.fr>
2022-03-29 20:44:13 +0200

    test recharts

--- front/src/components/dashboard/stats/dailyChart.jsx
+++ front/src/components/dashboard/stats/dailyChart.jsx
@@ -1,13 +1,63 @@
 import { Component } from "react"
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts"
+
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+]
 
 export default class DailyChart extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      data: null,
-      /* loading: true,
-      error: null, */
-    }
+    this.state = { data: null }
   }
 
   componentDidMount() {
@@ -23,35 +73,38 @@ export default class DailyChart extends Component {
       return response.data.data
     })
     .then(data => {
-      this.setState({
-        data: data,
-        /* error: null, */
-      })
-      // console.log(this.state)
+      this.setState({ data: data })
     })
     .catch(error => {
       console.log(error)
     })
-    // .finally(this.setState({ loading: false }))
   }
 
+  // console.log(this.state.data.sessions)
+
   render() {
-      return(
+    return(
       <div>
         { this.state.data &&
-          <>
-            <h2>Activité quotidienne</h2>
-            <ul>
-              { this.state.data.sessions.map(
-                session =>
-                  <li>
-                    Jour : {session.day}<br />
-                    Poids : {session.kilogram}<br />
-                    Calories : {session.calories}
-                  </li>
-              ) }
-            </ul>
-          </>
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
         }
       </div>
     )
--- front/src/components/dashboard/stats/performanceChart.jsx
+++ front/src/components/dashboard/stats/performanceChart.jsx
@@ -1,4 +1,50 @@
 import { Component } from "react"
+import {
+  Radar,
+  RadarChart,
+  PolarGrid,
+  PolarAngleAxis,
+  PolarRadiusAxis
+} from "recharts"
+
+const data = [
+  {
+    subject: "Math",
+    A: 120,
+    B: 110,
+    fullMark: 150
+  },
+  {
+    subject: "Chinese",
+    A: 98,
+    B: 130,
+    fullMark: 150
+  },
+  {
+    subject: "English",
+    A: 86,
+    B: 130,
+    fullMark: 150
+  },
+  {
+    subject: "Geography",
+    A: 99,
+    B: 100,
+    fullMark: 150
+  },
+  {
+    subject: "Physics",
+    A: 85,
+    B: 90,
+    fullMark: 150
+  },
+  {
+    subject: "History",
+    A: 65,
+    B: 85,
+    fullMark: 150
+  }
+]
 
 export default class PerformanceChart extends Component {
   constructor(props) {
@@ -20,26 +66,37 @@ export default class PerformanceChart extends Component {
     })
     .then(data => {
       this.setState({ data: data })
-      // console.log(this.state)
     })
     .catch(error => {
       console.log(error)
     })
   }
 
+  // console.log(this.state.data.data)
+
   render() {
     return(
       <div>
         { this.state.data &&
-          <>
-            <h2>Radar Chart</h2>
-            <ul>
-              { this.state.data.data.map(
-                session => 
-                  <li>{session.value}</li>
-              ) }
-            </ul>
-          </>
+          <RadarChart
+            cx={300}
+            cy={250}
+            outerRadius={150}
+            width={500}
+            height={500}
+            data={data}
+          >
+            <PolarGrid />
+            <PolarAngleAxis dataKey="subject" />
+            <PolarRadiusAxis />
+            <Radar
+              name="Mike"
+              dataKey="A"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+          </RadarChart>
         }
       </div>
     )
