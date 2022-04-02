d75cba3e1a226aa852cd74aa5911bf7b834db373
Kevin Junger <kevin.junger@outlook.fr>
2022-04-02 17:18:52 +0200

    chart score

--- front/src/components/stats/goalChart.jsx
+++ front/src/components/stats/goalChart.jsx
@@ -1,56 +1,10 @@
 import { Component } from "react"
-import { RadialBarChart, RadialBar, Legend } from "recharts"
-
-const data = [
-  {
-    name: "18-24",
-    uv: 31.47,
-    pv: 2400,
-    fill: "#8884d8"
-  },
-  {
-    name: "25-29",
-    uv: 26.69,
-    pv: 4567,
-    fill: "#83a6ed"
-  },
-  {
-    name: "30-34",
-    uv: 15.69,
-    pv: 1398,
-    fill: "#8dd1e1"
-  },
-  {
-    name: "35-39",
-    uv: 8.22,
-    pv: 9800,
-    fill: "#82ca9d"
-  },
-  {
-    name: "40-49",
-    uv: 8.63,
-    pv: 3908,
-    fill: "#a4de6c"
-  },
-  {
-    name: "50+",
-    uv: 2.63,
-    pv: 4800,
-    fill: "#d0ed57"
-  },
-  {
-    name: "unknow",
-    uv: 6.67,
-    pv: 4800,
-    fill: "#ffc658"
-  }
-]
-
-const style = {
-  top: 0,
-  left: 350,
-  lineHeight: "24px"
-}
+import {
+  RadialBarChart,
+  RadialBar,
+  ResponsiveContainer,
+  PolarAngleAxis
+} from "recharts"
 
 export default class GoalChart extends Component {
   constructor(props) {
@@ -71,45 +25,55 @@ export default class GoalChart extends Component {
       return response.data.data
     })
     .then(data => {
-      this.setState({ data: data })
+      this.setState({
+        data: [
+          { value: data.todayScore * 100 }
+        ]
+      })
     })
     .catch(error => {
       console.log(error)
     })
   }
 
-  // console.log(this.state.data.todayScore)
-
   render() {
     return(
-      <div>
+      <div className="goal">
         { this.state.data &&
-          <RadialBarChart
-            width={500}
-            height={300}
-            cx={150}
-            cy={150}
-            innerRadius={20}
-            outerRadius={140}
-            barSize={10}
-            data={data}
-          >
-            <RadialBar
-              minAngle={15}
-              label={{ position: "insideStart", fill: "#fff" }}
-              background
-              clockWise
-              dataKey="uv"
-            />
-            <Legend
-              iconSize={10}
-              width={120}
-              height={140}
-              layout="vertical"
-              verticalAlign="middle"
-              wrapperStyle={style}
-            />
-          </RadialBarChart>
+          <>
+            <h2>Score</h2>
+            <ResponsiveContainer width="100%" height="80%">
+              <RadialBarChart
+                innerRadius={"95%"}
+                barSize={10}
+                data={this.state.data}
+                startAngle={225}
+                endAngle={-45}
+              >
+                <PolarAngleAxis
+                  type="number"
+                  domain={[0, 100]}
+                  angleAxisId={0}
+                  tick={false}
+                />
+                <RadialBar
+                  background
+                  clockWise
+                  dataKey="value"
+                  fill="#ff0000"
+                  cornerRadius={50}
+                />
+                <text
+                  x={"50%"}
+                  y={"50%"}
+                  textAnchor="middle"
+                  dominantBaseline="middle"
+                >
+                  couille
+                </text>
+              </RadialBarChart>
+            </ResponsiveContainer>
+          </>
         }
       </div>
     )
