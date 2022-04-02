e2689efe57876fbdff02b46122b9d1e0fcfac210
Kevin Junger <kevin.junger@outlook.fr>
2022-04-02 15:40:02 +0200

    chart durée moyenne sessions

--- front/src/components/stats/sessionsChart.jsx
+++ front/src/components/stats/sessionsChart.jsx
@@ -3,56 +3,9 @@ import {
   LineChart,
   Line,
   XAxis,
-  YAxis,
-  CartesianGrid,
   Tooltip,
-  Legend
+  ResponsiveContainer
 } from "recharts"
-
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
-]
 
 export default class SessionsChart extends Component {
   constructor(props) {
@@ -80,36 +33,44 @@ export default class SessionsChart extends Component {
     })
   }
 
-  // console.log(this.state.data.sessions)
-
   render() {
     return(
-      <div>
+      <div className="sessions">
         { this.state.data &&
-          <LineChart
-            width={500}
-            height={300}
-            data={data}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 20,
-              bottom: 5
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Line
-              type="monotone"
-              dataKey="pv"
-              stroke="#8884d8"
-              activeDot={{ r: 8 }}
-            />
-            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-          </LineChart>
+          <>
+            <h2>Durée moyenne des sessions</h2>
+            <ResponsiveContainer width="100%" height="70%">
+              <LineChart
+                data={this.state.data.sessions}
+                margin={{ top: 0, right: 24, bottom: 32, left: 24 }}
+              >
+                <XAxis
+                  dataKey="day"
+                  stroke="rgba(255, 255, 255, 0.6)"
+                  axisLine={false}
+                  dy={10}
+                  tickLine={false}
+                  tick={{
+                    fontSize: 12,
+                    fontWeight: 500,
+                  }}
+                />
+                <Tooltip />
+                <Line
+                  type="monotone"
+                  dataKey="sessionLength"
+                  stroke="rgba(255, 255, 255, 0.6)"
+                  strokeWidth={2}
+                  dot={false}
+                  activeDot={{
+                    stroke: "rgba(255,255,255, 0.6)",
+                    strokeWidth: 10,
+                    r: 5,
+                  }}
+                />
+              </LineChart>
+            </ResponsiveContainer>
+          </>
         }
       </div>
     )
