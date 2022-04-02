400f901e788d0441dc4fb7acef5cbf966cc0ed81
Kevin Junger <kevin.junger@outlook.fr>
2022-04-02 15:59:35 +0200

    chart performance et css

--- front/src/components/stats/performanceChart.jsx
+++ front/src/components/stats/performanceChart.jsx
@@ -4,47 +4,8 @@ import {
   RadarChart,
   PolarGrid,
   PolarAngleAxis,
-  PolarRadiusAxis
+  ResponsiveContainer,
 } from "recharts"
-
-const data = [
-  {
-    subject: "Math",
-    A: 120,
-    B: 110,
-    fullMark: 150
-  },
-  {
-    subject: "Chinese",
-    A: 98,
-    B: 130,
-    fullMark: 150
-  },
-  {
-    subject: "English",
-    A: 86,
-    B: 130,
-    fullMark: 150
-  },
-  {
-    subject: "Geography",
-    A: 99,
-    B: 100,
-    fullMark: 150
-  },
-  {
-    subject: "Physics",
-    A: 85,
-    B: 90,
-    fullMark: 150
-  },
-  {
-    subject: "History",
-    A: 65,
-    B: 85,
-    fullMark: 150
-  }
-]
 
 export default class PerformanceChart extends Component {
   constructor(props) {
@@ -72,31 +33,33 @@ export default class PerformanceChart extends Component {
     })
   }
 
-  // console.log(this.state.data.data)
-
   render() {
     return(
-      <div>
+      <div className="performance">
         { this.state.data &&
-          <RadarChart
-            cx={300}
-            cy={250}
-            outerRadius={150}
-            width={500}
-            height={500}
-            data={data}
-          >
-            <PolarGrid />
-            <PolarAngleAxis dataKey="subject" />
-            <PolarRadiusAxis />
-            <Radar
-              name="Mike"
-              dataKey="A"
-              stroke="#8884d8"
-              fill="#8884d8"
-              fillOpacity={0.6}
-            />
-          </RadarChart>
+          <ResponsiveContainer width="100%" height="80%">
+            <RadarChart
+              outerRadius={"90%"}
+              data={this.state.data.data}
+            >
+              <PolarGrid radialLines={false} />
+              <PolarAngleAxis
+                dataKey="kind"
+                stroke="white"
+                dy={4}
+                tickLine={false}
+                tick={{
+                  fontSize: 12,
+                  fontWeight: 500,
+                }} />
+              <Radar
+                dataKey="value"
+                stroke="transparent"
+                fill="#ff0000"
+                fillOpacity={0.7}
+              />
+            </RadarChart>
+          </ResponsiveContainer>
         }
       </div>
     )
