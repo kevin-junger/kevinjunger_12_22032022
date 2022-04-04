c1fd4fa3487f818f6f790cd75ddbfd07324b5d4e
Kevin Junger <kevin.junger@outlook.fr>
2022-04-04 14:22:30 +0200

    passage sidebar et conteneur main en styled-components

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,20 @@
 import Header from "./components/header"
 import Sidebar from "./components/sidebar"
 import Dashboard from "./components/dashboard"
+import styled from "styled-components"
+
+const Main = styled.main`
+  display: flex;
+`
 
 export default function App() {
   return (
     <>
       <Header />
-      <main className="main">
+      <Main>
         <Sidebar />
         <Dashboard />
-      </main>
+      </Main>
     </>
   )
 }
--- front/src/components/sidebar.jsx
+++ front/src/components/sidebar.jsx
@@ -2,18 +2,48 @@ import BicycleIcon from "../assets/icons/bicycle"
 import SwimIcon from "../assets/icons/swim"
 import WeightIcon from "../assets/icons/weight"
 import YogaIcon from "../assets/icons/yoga"
-import "../css/sidebar.css"
+import styled from "styled-components"
 
-export default function Sidebar() {
+const Sidebar = styled.aside`
+  height: calc(100vh - 90px);
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`
+
+const SidebarMenu = styled.ul`
+  margin: 1.5rem;
+  padding: 0;
+  list-style: none;
+`
+
+const SidebarMenuElt = styled.li`
+  margin-bottom: 1rem;
+  background-color: white;
+  padding: 1rem;
+  border-radius: 10px;
+`
+
+const Copyright = styled.p`
+  margin-top: 3rem;
+  margin-bottom: 2rem;
+  color: white;
+  writing-mode: vertical-lr;
+  text-orientation: sideways-right;
+`
+
+export default function SidebarUI() {
   return(
-    <aside className="sidebar">
-      <ul className="sidebar__menu">
-        <li><YogaIcon /></li>
-        <li><SwimIcon /></li>
-        <li><BicycleIcon /></li>
-        <li><WeightIcon /></li>
-      </ul>
-      <p className="sidebar__copyright">(c) SportSee - 2022</p>
-    </aside>
+    <Sidebar>
+      <SidebarMenu>
+        <SidebarMenuElt><YogaIcon /></SidebarMenuElt>
+        <SidebarMenuElt><SwimIcon /></SidebarMenuElt>
+        <SidebarMenuElt><BicycleIcon /></SidebarMenuElt>
+        <SidebarMenuElt><WeightIcon /></SidebarMenuElt>
+      </SidebarMenu>
+      <Copyright>(c) SportSee - 2022</Copyright>
+    </Sidebar>
   )
 }
