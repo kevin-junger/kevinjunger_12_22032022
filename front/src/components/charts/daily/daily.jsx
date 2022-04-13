167d4b14fdf93d40a67203507c2ab7fa9f0b7633
Kevin Junger <kevin.junger@outlook.fr>
2022-04-13 22:32:33 +0200

    chart daily - passage classe à fonction

--- front/src/components/charts/daily/daily.jsx
+++ front/src/components/charts/daily/daily.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import GetUserApi from "../../../containers/dashboard/getUserApi"
 import {
@@ -46,29 +46,22 @@ const CustomTooltip = ({ active, payload }) => {
 }
 
 /**
- * @class Daily
- * @classdesc Displays the analytics for the user's daily activity via a bar chart
+ * Displays the analytics for the user's daily activity via a bar chart
  * @param { GetUserApi } api - Mandatory
+ * @returns { StyledComponent }
  */
-export default class Daily extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      data: null,
-      error: false,
-    }
-  }
+export default function Daily(props) {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(false)
 
-  static propTypes = {
-    api: PropTypes.instanceOf(GetUserApi).isRequired,
-  }
+  const api = props.api
 
   /**
    * Fetches and stores the user's daily activity over the last 7 days
    */
-  componentDidMount() {
-    this.props.api.getUserActivity()
+  useEffect(() => {
+    api.getUserActivity()
     .then(response => {
       if(response.statusText !== "OK") {
         throw new Error(response.statusText)
@@ -88,96 +81,92 @@ export default class Daily extends Component {
         })
       })
       
-      this.setState({
-        loading: false,
-        data: sessions,
-        error: false,
-      })
+      setLoading(false)
+      setData(sessions)
     })
     .catch(error => {
       console.log(error)
-      this.setState({
-        loading: false,
-        data: null,
-        error: true,
-      })
+      setLoading(false)
+      setError(true)
     })
-  }
+  }, [api])
 
-  render() {
-    return(
-      <>
-        { this.state.loading &&
-          <Container>
-            <Loader />
-          </Container>
-        }
-        { this.state.data &&
-          <Stat>
-            <Header>
-              <Title>Activité quotidienne</Title>
-              <Legend>
-                <LegendColor>
-                  <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#282D30"/>
-                  </svg>
-                </LegendColor>
-                <LegendTitle>Poids (kg)</LegendTitle>
-                <LegendColor>
-                  <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#E60000"/>
-                  </svg>
-                </LegendColor>
-                <LegendTitle>Calories brûlées (kCal)</LegendTitle>
-              </Legend>
-            </Header>
-            <ResponsiveContainer width="100%" height="100%">
-              <BarChart
-                data={this.state.data}
-                margin={{ top: 80, right: 24, bottom: 32, left: 24 }}
-                barGap={8}
-                barCategoryGap="40%"
-              >
-                <CartesianGrid
-                  strokeDasharray="3 3"
-                  vertical={false}
-                />
-                <XAxis 
-                  dataKey="day"
-                  dy={16}
-                  tickLine={false}
-                  tick={{ fontSize: 14, fontWeight: 500 }}
-                />
-                <YAxis
-                  dataKey="calories"
-                  hide={true}
-                />
-                <Bar
-                  dataKey="kilogram"
-                  fill="#000000"
-                  radius={[50, 50, 0, 0]}
-                />
-                <Bar
-                  dataKey="calories"
-                  fill="#ff0000"
-                  radius={[50, 50, 0, 0]}
-                />
-                <Tooltip
-                  cursor={{
-                    fill: "rgba(0, 0, 0, 0.1)",
-                  }}
-                  content={<CustomTooltip />}
-                />
-              </BarChart>
-            </ResponsiveContainer>
-          </Stat>
-        }
-        { this.state.error &&
-          <Container>
-            <Error>!</Error>
-          </Container>
-        }
-      </>
-    )
-  }
+  return(
+    <>
+      { loading &&
+        <Container>
+          <Loader />
+        </Container>
+      }
+      { data &&
+        <Stat>
+          <Header>
+            <Title>Activité quotidienne</Title>
+            <Legend>
+              <LegendColor>
+                <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#282D30"/>
+                </svg>
+              </LegendColor>
+              <LegendTitle>Poids (kg)</LegendTitle>
+              <LegendColor>
+                <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path fillRule="evenodd" clipRule="evenodd" d="M4 8C6.20914 8 8 6.20914 8 4C8 1.79086 6.20914 0 4 0C1.79086 0 0 1.79086 0 4C0 6.20914 1.79086 8 4 8Z" fill="#E60000"/>
+                </svg>
+              </LegendColor>
+              <LegendTitle>Calories brûlées (kCal)</LegendTitle>
+            </Legend>
+          </Header>
+          <ResponsiveContainer width="100%" height="100%">
+            <BarChart
+              data={data}
+              margin={{ top: 80, right: 24, bottom: 32, left: 24 }}
+              barGap={8}
+              barCategoryGap="40%"
+            >
+              <CartesianGrid
+                strokeDasharray="3 3"
+                vertical={false}
+              />
+              <XAxis 
+                dataKey="day"
+                dy={16}
+                tickLine={false}
+                tick={{ fontSize: 14, fontWeight: 500 }}
+              />
+              <YAxis
+                dataKey="calories"
+                hide={true}
+              />
+              <Bar
+                dataKey="kilogram"
+                fill="#000000"
+                radius={[50, 50, 0, 0]}
+              />
+              <Bar
+                dataKey="calories"
+                fill="#ff0000"
+                radius={[50, 50, 0, 0]}
+              />
+              <Tooltip
+                cursor={{
+                  fill: "rgba(0, 0, 0, 0.1)",
+                }}
+                content={<CustomTooltip />}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </Stat>
+      }
+      { error &&
+        <Container>
+          <Error>!</Error>
+        </Container>
+      }
+    </>
+  )
+}
+
+Daily.propTypes = {
+  api: PropTypes.instanceOf(GetUserApi).isRequired
 }
