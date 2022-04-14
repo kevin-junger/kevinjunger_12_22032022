9799e1f3267a02c80a48a30a61e6b546e2c2b0cc
Kevin Junger <kevin.junger@outlook.fr>
2022-04-14 10:15:57 +0200

    chart performance - passage classe à fonction

--- front/src/components/charts/performance/performance.jsx
+++ front/src/components/charts/performance/performance.jsx
@@ -1,44 +1,23 @@
-import { Component } from "react"
+import { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import GetUserApi from "../../../containers/dashboard/getUserApi"
-import {
-  Container,
-  Loader,
-  Error,
-  Stat,
-} from "./performanceUI"
-import {
-  Radar,
-  RadarChart,
-  PolarGrid,
-  PolarAngleAxis,
-  ResponsiveContainer,
-} from "recharts"
+import { Container, Loader, Error, Stat } from "./performanceUI"
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, ResponsiveContainer } from "recharts"
 
 /**
- * @class Performance
- * @classdesc Displays the analytics for the user's performance stats
+ * Displays the analytics for the user's performance stats
  * @param { GetUserApi } api - Mandatory
+ * @returns { Component | null }
  */
-export default class Performance extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      data: null,
-      error: false,
-    }
-  }
+export default function Performance(props) {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(false)
 
-  static propTypes = {
-    api: PropTypes.instanceOf(GetUserApi).isRequired,
-  }
+  const api = props.api
 
-  /**
-   * Fetches and stores the user's performance indexes
-   */
-  componentDidMount() {
-    this.props.api.getUserPerformance()
+  useEffect(() => {
+    api.getUserPerformance()
     .then(response => {
       if(response.statusText !== "OK") {
         throw new Error(response.statusText)
@@ -47,7 +26,7 @@ export default class Performance extends Component {
       return response.data.data
     })
     .then(data => {
-      const perfs = []
+      let perfs = []
 
       const kinds = ['Cardio', 'Energie', 'Endurance', 'Force', 'Vitesse', 'Intensité']
 
@@ -62,64 +41,43 @@ export default class Performance extends Component {
         })
       }
 
-      this.setState({
-        loading: false,
-        data: perfs,
-        error: false,
-      })
+      setLoading(false)
+      setData(perfs)
     })
     .catch(error => {
       console.log(error)
-      this.setState({
-        loading: false,
-        data: null,
-        error: true,
-      })
+      setLoading(false)
+      setError(true)
     })
-  }
+  }, [api])
 
-  render() {
-    return(
-      <>
-        { this.state.loading &&
-          <Container>
-            <Loader />
-          </Container>
-        }
-        { this.state.data &&
-          <Stat>
-            <ResponsiveContainer width="100%" height="100%">
-              <RadarChart
-                outerRadius={"60%"}
-                innerRadius={"0"}
-                data={this.state.data}
-              >
-                <PolarGrid radialLines={false} />
-                <PolarAngleAxis
-                  dataKey="kind"
-                  stroke="white"
-                  dy={4}
-                  tickLine={false}
-                  tick={{
-                    fontSize: 12,
-                    fontWeight: 500,
-                  }} />
-                <Radar
-                  dataKey="value"
-                  stroke="transparent"
-                  fill="#ff0000"
-                  fillOpacity={0.7}
-                />
-              </RadarChart>
-            </ResponsiveContainer>
-          </Stat>
-        }
-        { this.state.error &&
-          <Container>
-            <Error>!</Error>
-          </Container>
-        }
-      </>
-    )
-  }
+  return(
+    <>
+      { loading &&
+        <Container>
+          <Loader />
+        </Container>
+      }
+      { data &&
+        <Stat>
+          <ResponsiveContainer width="100%" height="100%">
+            <RadarChart outerRadius={"60%"} innerRadius={"0"} data={data}>
+              <PolarGrid radialLines={false} />
+              <PolarAngleAxis dataKey="kind" stroke="white" dy={4} tickLine={false} tick={{ fontSize: 12, fontWeight: 500, }} />
+              <Radar dataKey="value" stroke="transparent" fill="#ff0000" fillOpacity={0.7} />
+            </RadarChart>
+          </ResponsiveContainer>
+        </Stat>
+      }
+      { error &&
+        <Container>
+          <Error>!</Error>
+        </Container>
+      }
+    </>
+  )
+}
+
+Performance.propTypes = {
+  api: PropTypes.instanceOf(GetUserApi).isRequired,
 }
